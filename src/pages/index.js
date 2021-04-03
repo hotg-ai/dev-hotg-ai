1658ee697fc2c65c3f6b378c087a24f143be92bc
Kartik Thakore <kthakore@alumni.uwo.ca>
2021-04-03 10:57:01 -0700

    Added more cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,9 +61,9 @@ export default function Home() {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <header className={clsx('hero ', styles.heroBanner)}>
         <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
+          <h1 className="hero__title hero__title--primary">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
             <Link
