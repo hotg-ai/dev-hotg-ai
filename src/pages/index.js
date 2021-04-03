56de2d7b212dec0b82152ef12e01d417e583be8c
Kartik Thakore <kthakore@alumni.uwo.ca>
2021-04-03 11:02:38 -0700

    Simple styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,14 +61,14 @@ export default function Home() {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <header className={clsx('hero ', styles.heroBanner)}>
+      <header className={clsx('hero hero--dark', styles.heroBanner)}>
         <div className="container">
-          <h1 className="hero__title hero__title--primary">{siteConfig.title}</h1>
+          <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
             <Link
               className={clsx(
-                'button button--outline button--secondary button--lg',
+                'button button--outline button--primary button--lg',
                 styles.getStarted,
               )}
               to={useBaseUrl('docs/')}>
