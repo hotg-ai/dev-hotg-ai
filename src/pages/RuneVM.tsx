6c60f0682e802bf0c3e2c0eda9bc17936e947324
Kartik Thakore <kthakore@alumni.uwo.ca>
2021-04-03 17:07:52 -0700

    Added runeVM

---
+++ src/pages/RuneVM.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+function RuneVM() {
+  return (
+    <Layout title="RuneVM">
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '50vh',
+          fontSize: '20px',
+        }}>
+        <p>
+          RuneVM
+        </p>
+      </div>
+    </Layout>
+  );
+}
+
+export default RuneVM;
