d9258e4033ca0d85f93a5e4d9ee970b6bc738091
Kartik Thakore <kthakore@alumni.uwo.ca>
2021-05-25 09:22:43 -0700

    Making build add the firebase stuff too

--- merge_firebase.build.js
+++ merge_firebase.build.js
@@ -1,13 +1,15 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
+const fs = require('fs')
 
-JSDOM.fromFile('build/index.html')
+JSDOM.fromFile('build/index.html.bak')
     .then((dom) => {
       let body = dom.window.document.getElementsByTagName('body');
 
       JSDOM.fromFile('firebase.conf.html').then((dom_fb) => {
-            let body_fb = dom.window.document.getElementsByTagName('body');
+            let body_fb = dom_fb.window.document.getElementsByTagName('body');
             body[0].innerHTML = body[0].innerHTML + body_fb[0].innerHTML
+            fs.writeFileSync('build/index.html', dom.serialize());
       })
-      console.log(dom.serialize());
+      
     });
