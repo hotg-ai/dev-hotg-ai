08854d19c1089fcc839e2048be968349b70fa82c
f0rodo <56056337+f0rodo@users.noreply.github.com>
2022-02-02 07:06:38 -0800

    Added sidebarjs

--- sidebars.js
+++ sidebars.js
@@ -10,20 +10,33 @@ const lessons = fs.readdirSync(tutorials)
 
 module.exports = {
   docs: [
+    // Videos for overview of the system
     {
       type: 'doc',
       id: 'setup'
     },
     {
       type: 'doc',
-      id: 'getting_started',
+      id: 'getting_started', //change this to studio => https://studio.hotg.ai => workshop link => make you apps: Read more => Forge SDK or Learn about Rune goes to the current stuff
+
     },
+    // {
+    //   type: 'category',
+    //   id: 'runtimes',
+    //   label: 'Forge SDK'
+    // },
+    // {
+    //   type: 'category',
+    //   id: 'runtimes',
+    //   label: 'Rune'
+    // },
     {
       type: 'category',
       label: 'Tutorial',
       collapsed: true,
       items: lessons
     },
+    
     {
       type: 'doc',
       id: 'overview'
